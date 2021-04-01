fa381fe157daa88bf2df923c41cf6ed150cca5dd
graeeee <46288884+graeeee@users.noreply.github.com>
2021-04-01 09:34:33 -0400

    Create myToken.py

---
+++ myToken.py
@@ -0,0 +1,9 @@
+token = 'blank for safety'
+# PLEASE USE INTEGERS NOT STRINGS. Grab the server ID and channel IDs for the channels you wish to use. 
+guildID = 698321471881347094
+team1ChannelId = 698323152836493312
+team2ChannelId = 698323212773228584
+setupChannelId = 698328368676077698
+csRoleID = 796195408920444951
+prefix='!'
+description='A bot for organizing 10 man pugs/scrims for various 5v5 competitive games.'
